466f48edfebd1642145043366efdaf974648f952
Dion <dionysius.indraatmadja@gmail.com>
2025-04-22 12:26:25 -0400

    Quartz sync: Apr 22, 2025, 12:26 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "dion's garden",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -23,7 +23,7 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
+        header: "Source Sans Pro", // header: "Schibsted Grotesk", 
         body: "Source Sans Pro",
         code: "IBM Plex Mono",
       },
